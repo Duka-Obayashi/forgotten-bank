7bac1a5b0d66d91429cb6dbdee81d17f686d5ee3
eduardo_Obayashi <eduardo.obayashi@gmail.com>
2022-09-18 23:15:22 -0300

    Cria funções de interação com as Accounts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use forgotten_bank::db::*;
+
 fn main() {
     println!("Hello, world!");
+
+    accounts::update_character(3, 555);
+
+    let accounts = accounts::list_accounts();
+
+    println!("Displaying {} posts", accounts.len());
+    for account in accounts {
+        println!("{} --> {}", account.id, account.character_id);
+        println!("-----------\n");
+    }
+
 }
